9c051c9ba2c93a4741ede52cadab646978352009
HOHOKIM <164991647+HOHOKIM@users.noreply.github.com>
2026-06-24 14:16:21 +0900

    Initial commit

---
+++ src/main/java/com/yaho/factchecker/FactcheckerApplication.java
@@ -0,0 +1,13 @@
+package com.yaho.factchecker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FactcheckerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FactcheckerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yaho/factchecker/FactcheckerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yaho.factchecker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FactcheckerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
