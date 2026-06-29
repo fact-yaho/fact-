5ce6892bb61bee78d6465ade95f395e297b2ffff
chanhongkwak <rhkrcksghd1@gmail.com>
2026-06-29 20:53:46 +0900

    fix:코드래빗 리뷰 반영

--- src/main/java/com/yaho/factchecker/global/exception/GlobalExceptionHandler.java
+++ src/main/java/com/yaho/factchecker/global/exception/GlobalExceptionHandler.java
@@ -2,6 +2,9 @@ package com.yaho.factchecker.global.exception;
 
 import com.yaho.factchecker.global.response.ErrorResponse;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.validation.BindException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -17,6 +20,19 @@ public class GlobalExceptionHandler {
                 .body(ErrorResponse.from(errorCode));
     }
 
+    @ExceptionHandler({
+            MethodArgumentNotValidException.class,
+            BindException.class,
+            HttpMessageNotReadableException.class
+    })
+    public ResponseEntity<ErrorResponse> handleInvalidInputException(Exception e) {
+        ErrorCode errorCode = ErrorCode.INVALID_INPUT_VALUE;
+
+        return ResponseEntity
+                .status(errorCode.getStatus())
+                .body(ErrorResponse.from(errorCode));
+    }
+
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ErrorResponse> handleException(Exception e) {
         ErrorCode errorCode = ErrorCode.INTERNAL_SERVER_ERROR;
@@ -25,4 +41,5 @@ public class GlobalExceptionHandler {
                 .status(errorCode.getStatus())
                 .body(ErrorResponse.from(errorCode));
     }
+
 }
