0194601b004626f9af8a1396d8a6e1db25052a45
SeongWon Jeong <newon.jsw@gmail.com>
2026-06-30 16:59:07 +0900

    refactor: move location, rename and add verdict types

--- src/main/java/com/yaho/factchecker/domain/result/code/Verdict.java
+++
@@ -1,9 +0,0 @@
-package com.yaho.factchecker.domain.result.code;
-
-public enum Verdict {
-    TRUE,
-    MOSTLY_TRUE,
-    UNCERTAIN,
-    MOSTLY_FALSE,
-    FALSE
-}
---
+++ src/main/java/com/yaho/factchecker/global/type/Verdict.java
@@ -0,0 +1,11 @@
+package com.yaho.factchecker.global.type;
+
+public enum Verdict {
+    CONSISTENT,
+    PARTIALLY_CONSISTENT,
+    UNCERTAIN,
+    PARTIALLY_INCONSISTENT,
+    INCONSISTENT,
+    INSUFFICIENT,
+    OUT_OF_SCOPE
+}
